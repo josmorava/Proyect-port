fda4aa0ce05e315aca95a25df2b1fd2f5a7a2b25
josmorava <josemorales-1234@hotmail.es>
2020-10-23 22:40:42 -0400

    Agregando cards de MATERAILIZE a perfil.html

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+        'primary': '#2d3c65',
+        'secondary' : '#b9c5d4'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
